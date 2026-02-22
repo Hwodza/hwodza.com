67dfafa52201bbd064b1966e62b68c807dff3d8a
Henry <hwodza@gmail.com>
2026-02-21 21:05:27 -0500

    imported style in places

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,14 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://odza.dev',
+  site: 'https://www.odza.dev',
   base: '/',
   vite: {
     build: {
       minify: true
     }
   },
+  output: 'server',
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
