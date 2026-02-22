892394802f9f1d43588b9616d534f315ec77838a
Henry <hwodza@gmail.com>
2026-02-21 21:53:33 -0500

    added some stuff to the astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ const isDev = process.env.NODE_ENV === 'development';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://odza.dev',
+  output: 'server',
   adapter: isDev ? undefined : cloudflare()
 });
